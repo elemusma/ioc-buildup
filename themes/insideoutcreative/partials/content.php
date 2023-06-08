0fb464f8dd2b64f91d77a506a7986e05a3c8e209
Efrain Lemus <efraintlemusm@gmail.com>
2023-06-08 15:02:42 -0500

    fix youtube api and resources

--- themes/insideoutcreative/partials/content.php
+++ themes/insideoutcreative/partials/content.php
@@ -18,7 +18,7 @@ if($layout == 'Documents & Resources'){
         
     if($title){
         echo '<div class="position-absolute" style="top:50%;left:50%;transform:translate(-50%,-80%);">';
-        echo '<h1 class="text-white bold mb-0" style="font-size:5vw;">' . $title . '</h1>';
+        echo '<h1 class="text-white bold" style="font-size:5vw;margin:0;">' . $title . '</h1>';
         echo '</div>';
     }
     echo '<div class="container-fluid">';
@@ -44,15 +44,15 @@ if($layout == 'Documents & Resources'){
     // echo '<div>';
 
     echo '<div class="d-flex justify-content-center position-relative z-3 h-100" style="">';
-    echo '<div class="p-2 d-inline-block mr-3" style="border-radius:50%;border:1px solid var(--accent-secondary);height:35px;width:35px;">';
+    echo '<div class="p-2 mr-3 d-flex align-items-center justify-content-center" style="border-radius:50%;border:1px solid var(--accent-secondary);height:35px;min-width:35px;">';
     echo '<div style="width:15px;height:18px;">';
     echo '<svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 448 512"><path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/></svg>';
     echo '</div>';
     echo '</div>';
 
-    echo '<div class="">';
-    echo '<span class="col-resources-title text-white" style="font-size:1.25rem;">' . esc_html( $link_title ) . '</span>';
-    echo '<div class="bg-accent mt-3" style="height:8px;width:75px;"></div>';
+    echo '<div class="" style="padding-left:15px;">';
+    echo '<span class="text-white " style="font-size:1.25rem;">' . esc_html( $link_title ) . '</span>';
+    echo '<div class="bg-accent mt-3" style="height:8px;width:75px;margin-top:10px;"></div>';
 
     echo '<div class="resources-col-description text-white pt-2 small">';
     echo get_sub_field('description');
